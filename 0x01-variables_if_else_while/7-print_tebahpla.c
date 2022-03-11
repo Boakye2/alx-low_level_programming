41310c2cbe418da84de10e8ae3e263a06be95960
boakye2 <nanaboakyefred2@gmail.com>
2022-03-11 14:58:32 -0800

    brave

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+
+
+}
