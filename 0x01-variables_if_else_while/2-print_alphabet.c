a6e7f12039c8b5e13466a04f893feaa317f42b0c
boakye2 <nanaboakyefred2@gmail.com>
2022-03-11 11:30:15 -0800

    brave

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	return (0);
+
+
+}
