08bbfd5a07d54049b996850c936232653a428b89
boakye2 <nanaboakyefred2@gmail.com>
2022-03-11 12:46:37 -0800

    brave

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -16,8 +16,5 @@ int main(void)
 			putchar(c);
 	}
 	putchar('\n');
-
 	return (0);
-
-
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Prints numbers starting from 0.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c <= 10; c++)
+		printf("%d", c);
+
+	printf("\n");
+
+	return (0);
+
+
+}
