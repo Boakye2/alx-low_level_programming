e90660f96197707ad69cf9fba787309e4be3b68a
boakye2 <nanaboakyefred2@gmail.com>
2022-03-11 14:48:46 -0800

    brave

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Prints numbers starting from 0 with twice putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c < 10; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
