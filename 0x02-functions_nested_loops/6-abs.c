b23658a27721aec2b5f89dd922b10452de7f1ab7
boakye2 <nanaboakyefred2@gmail.com>
2022-03-15 17:48:11 -0700

    rav

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _abs - give the absolute value
+ *  @n: the number
+ *
+ *  Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
