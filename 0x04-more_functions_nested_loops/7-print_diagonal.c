2be3294e3955c6a77486f1f21f724909aba65794
boakye2 <nanaboakyefred2@gmail.com>
2022-03-17 11:56:28 -0700

    vox

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line in the terminal
+ * @n: the number of diagonal
+ * Return: the result
+ */
+void print_diagonal(int n)
+{
+	while (n > 0)
+	{
+		_putchar('\');
+		n = n - 1;
+	}
+
+	_putchar('\n');
+}
