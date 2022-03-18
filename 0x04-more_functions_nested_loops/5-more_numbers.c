7eb23da3714fae4cea80c2e0753b1d2a6709ed34
boakye2 <nanaboakyefred2@gmail.com>
2022-03-17 17:50:43 -0700

    vox

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,10 @@ void more_numbers(void)
 	int c;
 	int b;
 
-	for (b = 1; b <= 10; b++)
+	for (b = 0; b < 10; b++)
 	{
 		for (c = 0; c <= 14; c++)
-			_putchar(c);
+			_putchar(c + '0');
 	}
 
 	_putchar('\n');
