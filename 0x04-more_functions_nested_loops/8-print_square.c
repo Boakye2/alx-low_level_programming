ca07a6ef36fb9efd3c71248b5a46ca9259c1d6da
boakye2 <nanaboakyefred2@gmail.com>
2022-03-17 18:12:20 -0700

    vox

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,7 +17,7 @@ void print_square(int n)
 		for (a = 1; a <= size; a++)
 		{
 			_putchar('#');
-			for (b = 2; b <= size; b++)
+			for (b = 2; b <= n; b++)
 				_putchar('#');
 
 			_putchar('\n');
