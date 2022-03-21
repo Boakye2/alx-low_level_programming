1246b4a9386f58df11c49a41510a06dfb00ad912
boakye2 <nanaboakyefred2@gmail.com>
2022-03-21 15:02:23 -0700

    fn

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copy the string
+ * @a: a pointer
+ * @b: a pointer
+ */
+char *_strcpy(char *a, char *b)
+{
+	int i;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		a[i] = b[i];
+	}
+	a[i] = '\0';
+	return (a);
+}
