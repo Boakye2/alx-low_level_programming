511f2e49a2e1d408e1f3ce90ba8aaea34972e66a
boakye2 <nanaboakyefred2@gmail.com>
2022-03-23 03:32:15 -0700

    tgv

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest:first string
+ * @src: second string
+ * Return: a pointe to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s;
+
+	s = dest + src;
+	return (s);
+}
