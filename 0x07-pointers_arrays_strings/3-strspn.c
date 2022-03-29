f57922c8c3168f6b4e6c324c3829e8e4d1c1f31d
boakye2 <nanaboakyefred2@gmail.com>
2022-03-29 13:25:25 -0700

    fr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+/**
+ * _strspn - find length of prefix substring
+ * @s: string to check
+ * @a: substring of char
+ * Return: n bytes in intial segment of s
+ */
+unsigned int _strspn(char *s, char *a)
+{
+	int i;
+	int c;
+	int p;
+
+	while (*s)
+	{
+		i = 0;
+		p = c;
+		while (*(a + i) != '\0')
+		{
+			if (*(a + i) == *s)
+				c++;
+			i++;
+		}
+		if (p == c)
+			break;
+		s++;
+	}
+	return (c);
+}
