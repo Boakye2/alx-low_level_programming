582cb752b6f116dc8b40d009678ac90e4793c85d
boakye2 <nanaboakyefred2@gmail.com>
2022-03-29 10:23:22 -0700

    fr

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,15 @@
 /**
  * _memset - fills n bytes of memory
- *
  * @s: a pointer
  * @b: constant
  * @n: num bytes
- *
- * Return : pointer
+ * Return: pointer
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned in i = 0;
+	unsigned int i = 0;
 	char *beg = s;
-	
+
 	while (i < n)
 	{
 		*s++ = b;
