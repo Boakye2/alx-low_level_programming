ab18db65062901a1c1648bd14763c2af01ac9312
boakye2 <nanaboakyefred2@gmail.com>
2022-03-29 13:55:35 -0700

    fr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 unsigned int _strspn(char *s, char *a);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *s, char *b, unsigned int n);
 
