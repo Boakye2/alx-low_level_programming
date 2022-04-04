a1ead03f0508e44b25040ac499aa5c75c95d3d2c
boakye2 <nanaboakyefred2@gmail.com>
2022-04-04 13:05:36 -0700

    wake

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i, e;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	for (e = 0; e <= *str; e++)
+	{
+	}
+	e += 1;
+	ptr = malloc(sizeof(char) * e);
+
+	for (i = 0; i < e; i++)
+		ptr[i] = str[i];
+
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,9 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	ptr = malloc(sizeof(int) * height);
+
 	if (ptr == NULL)
 	{
 		free(ptr);
