0a560dfa5b8ed9a2e8b8f703fe48713029c33847
boakye2 <nanaboakyefred2@gmail.com>
2022-04-07 04:16:16 -0700

    lion

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - function
+ * @b: variable
+ * Return: the result
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - function
+ * @min: min
+ * @max: max
+ * Return: the result
+ */
+void *array_range(int min, int max)
+{
+	char *ptr;
+	int i, len;
+
+	if (min > max)
+		return (NULL);
+	len = max - min;
+	ptr = malloc(sizeof(int) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
