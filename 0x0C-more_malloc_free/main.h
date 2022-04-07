8fd997f77dd3f070e3ff9f9de517afb63ca98c21
boakye2 <nanaboakyefred2@gmail.com>
2022-04-07 16:44:25 -0700

    lion

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-int *array_range(int min, int max);
+void *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 #endif /* MAIN_H*/
