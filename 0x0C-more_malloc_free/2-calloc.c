3e4c27447ca6dba6309e4160727f393dfb2cddad
boakye2 <nanaboakyefred2@gmail.com>
2022-04-07 17:07:15 -0700

    lion

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 /**
  * _calloc - function
@@ -9,7 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	int i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
