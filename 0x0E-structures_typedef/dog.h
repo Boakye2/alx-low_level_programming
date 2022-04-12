24f986298dae5f594181ae727ce2f97f949df5eb
boakye2 <nanaboakyefred2@gmail.com>
2022-04-12 16:32:02 -0700

    croco

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog
 	char *owner;
 } dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void printf_dog(struct dog *d);
+void print_dog(struct dog *d);
 dog *new_dog(char *name, float age, char *owner);
 void free_dog(dog *d);
-#endif /* DOG_H*/
+#endif /* DOG_Hi*/
