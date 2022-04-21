4eab177f3b0ce90f042e97a6d35b4f3525139104
boakye2 <nanaboakyefred2@gmail.com>
2022-04-21 10:38:50 -0700

    Function that prints allelement of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_list - Prints all elemnts of list
+ * @h: the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node;
+
+	node = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * list_len - give the number of elements in a linked
+ * @h: the linked list
+ * Return: th e number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t el = 0;*
+	
+
+	while (h)
+	{
+		el++;
+		h = h->next;
+	}
+	return ();
+}
