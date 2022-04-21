58753920762cfffcbb51e1fc45125572d8ebf0d9
boakye2 <nanaboakyefred2@gmail.com>
2022-04-21 11:47:44 -0700

    Linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - Prints all elemnts of list
  * @h: the list
- * Return: the number of nodes
+ * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
 {
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 /**
  * list_len - give the number of elements in a linked
  * @h: the linked list
- * Return: th e number of elements
+ * Return: th e number of elements.
  */
 size_t list_len(const list_t *h)
 {
