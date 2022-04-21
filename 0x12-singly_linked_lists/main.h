4737fa1741babeecc5c99c274641e3d9f2632e24
boakye2 <nanaboakyefred2@gmail.com>
2022-04-21 10:58:50 -0700

    linked list

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+/**
+ * struct list_s - linked list
+ * @str: string
+ * @len: length
+ * @next: next node
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 #endif
