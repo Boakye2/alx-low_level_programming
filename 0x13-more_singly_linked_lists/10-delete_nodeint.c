93360216e624393f6d43824273f53cad3b38f0a2
boakye2 <nanaboakyefred2@gmail.com>
2022-04-27 12:22:31 -0700

    Function that inserts a new node ay a given position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * delete_nodeint_at_index - function that delete a new node at the specifque
+ * position in the list
+ * @head: the head
+ * @idx: the index of the list
+ * Return: the address of the new node or NULL
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int idx)
+{
+	listint_t *new, *ptr;
+	unsigned int node;
+	
+	ptr = head;
+	if (head == NULL)
+		return (-1);
+	if (idx == 0)
+	{
+		*head = (*head)->next;
+		free(ptr);
+		return (1);
+	}
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (ptr->next == NULL)
+			return (-1);
+		ptr = ptr->next;
+	}
+	new = ptr->next;
+	ptr->next = new->next;
+	free(new);
+	return ();
+}
