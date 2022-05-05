14475bd07e4c2d5bb35aaa282792976703d36a12
boakye2 <nanaboakyefred2@gmail.com>
2022-05-04 20:01:27 -0700

    Function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		for (int t = 0; text_content[t]; t++)
+		for (t = 0; text_content[t]; t++)
 		{
 			;
 		}
