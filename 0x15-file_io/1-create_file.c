5af371cca8caf17c54741f9cc140cba64b31df94
boakye2 <nanaboakyefred2@gmail.com>
2022-05-04 19:55:58 -0700

    Function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		for (; text_content[t]; t++)
 			;
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, t);
 
 	if (o == -1 ||  w == -1)
