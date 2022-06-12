a2a9cfa994fa20de4cd9e2180c8763c5e6f07030
Boakye2 <nanaboakyefred2@gmail.com>
2022-06-13 01:14:27 +0200

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,21 @@
-ss
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements.
+ * @h: The head of list.
+ *
+ * Return: The number of nodes .
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
