2cdae9a92c5faeaf5bde8f8675f1d3f0cbacddc5
Boakye2 <99751184+Boakye2@users.noreply.github.com>
2022-08-23 03:26:13 +0200

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+"""5-island_perimeter module"""
 
 
 def island_perimeter(grid):
-    """
-    island_perimeter: returns the preimeter of the island described in grid.
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
 
-    @grid(list): a list of lists of integers.
-    """
-    i = 0
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            if grid[y][x] == 1:
-                i += 4
-                if y > 0:
-                    if grid[y - 1][x] != 0:
-                        i -= 1
-                if y < len(grid) - 1:
-                    if grid[y + 1][x] != 0:
-                        i -= 1
-                if x > 0:
-                    if grid[y][x - 1] != 0:
-                        i -= 1
-                if x < len(grid[0]) - 1:
-                    if grid[y][x + 1] != 0:
-                        i -= 1
-    return i
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
+    
